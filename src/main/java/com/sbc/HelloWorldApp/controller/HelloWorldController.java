df0057f566872b216b9d13170fc3aa7a5ddba301
Surya Chettri <suny4evers@hotmail.com>
2022-03-10 13:38:45 -0600

    add files

---
+++ src/main/java/com/sbc/HelloWorldApp/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.sbc.HelloWorldApp.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		StringBuilder message = new StringBuilder("Hello World!");
+	    try {
+	      InetAddress ip = InetAddress.getLocalHost();
+	      message.append(" From host: " + ip);
+	    } catch (UnknownHostException e) {
+	      e.printStackTrace();
+	    }
+	      return message.toString();
+	    }
+}
---
+++ src/test/java/com/sbc/HelloWorldApp/HelloWorldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbc.HelloWorldApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
