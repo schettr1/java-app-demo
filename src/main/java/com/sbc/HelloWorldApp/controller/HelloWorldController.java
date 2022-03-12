fcfea2201627e45835161a2eeb5fcbe4e1e64ddc
schettr1 <suny4evers@hotmail.com>
2022-03-12 15:34:38 -0600

    Update HelloWorldController.java

--- src/main/java/com/sbc/HelloWorldApp/controller/HelloWorldController.java
+++ src/main/java/com/sbc/HelloWorldApp/controller/HelloWorldController.java
@@ -11,7 +11,8 @@ public class HelloWorldController {
 	
 	@RequestMapping("/")
 	public String hello() {
-		StringBuilder message = new StringBuilder("Hello World!");
+		//StringBuilder message = new StringBuilder("Hello World!");
+		StringBuilder message = new StringBuilder("Hello World Demo Application!");
 	    try {
 	      InetAddress ip = InetAddress.getLocalHost();
 	      message.append(" From host: " + ip);
